be96a59696c29d2c16dd52601f7c1c98745dd46b
tri <rinthu1997@gmail.com>
2019-12-24 14:59:29 +0700

    Add project files.

---
+++ AppComic/AppComic/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AppComic.Models;
+
+namespace AppComic.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
